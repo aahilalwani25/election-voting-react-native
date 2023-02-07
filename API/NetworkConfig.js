ed12d5921e69e4988c6d4b5536e9405d358481d0
aahilalwani25 <alwani.aahil25@gmail.com>
2023-02-08 00:56:59 +0500

    initial commit

---
+++ API/NetworkConfig.js
@@ -0,0 +1,3 @@
+const address= '192.168.8.100';
+console.log(address);
+module.exports={address};
---
+++ MVC/View/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View,Text } from 'react-native';
+import NavDrawer from '../../Navigation/Drawer';
+
+export default class Dashboard extends React.Component {
+
+    render(){
+        return(
+            <NavDrawer/>
+        );
+    }
+}
